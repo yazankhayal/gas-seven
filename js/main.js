bd23c66225fc390ea3808f4ad036bf0405378726
yazan.khayal <yazan.khayal@gmail.com>
2021-01-17 21:12:36 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,357 @@
+(function ($) {
+"use strict";
+
+// meanmenu
+    jQuery('#mobile-menu').meanmenu({
+        meanMenuContainer: '.mobile-menu',
+        meanScreenWidth: "991"
+    });
+
+
+// sticky
+var wind = $(window);
+var sticky = $('#sticky-header');
+wind.on('scroll', function () {
+	var scroll = wind.scrollTop();
+	if (scroll < 180) {
+		sticky.removeClass('sticky');
+	} else {
+		sticky.addClass('sticky');
+	}
+});
+
+
+// mainSlider
+
+    function mainSlider() {
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function(e, slick) {
+            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function(e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: false,
+            autoplaySpeed: 10000,
+            dots: true,
+            fade: true,
+			arrows:true,
+		    prevArrow:'<button type="button" class="slick-prev"> <i class="ti-angle-left"></i> </button>',
+            nextArrow:'<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+            responsive: [
+                { breakpoint: 767, settings: { dots: false, arrows: false } }
+            ]
+        });
+
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function() {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function() {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
+    }
+mainSlider();
+
+
+/* brand-active */
+$('.brand-active').owlCarousel({
+    loop:true,
+    nav:false,
+	dots:false,
+	autoplay:true,
+	navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:2
+        },
+        576:{
+            items:3
+        },
+        768:{
+            items:3
+        },
+        992:{
+            items:4
+        },
+        1200:{
+            items:6
+        }
+    }
+})
+
+/* brand2-active */
+$('.brand2-active').owlCarousel({
+    loop:true,
+    nav:false,
+	dots:false,
+	autoplay:true,
+	navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
+
+/* project-active */
+$('.project-active').owlCarousel({
+    stagePadding: 520,
+    loop:true,
+	center: true,
+    nav:true,
+	dots:true,
+    items:1,
+	navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+    responsive:{
+        0:{
+            stagePadding: 0,
+            items: 1,
+        },
+        768:{
+            items: 2,
+            stagePadding: 0,
+
+        },
+        992:{
+            items: 2,
+            stagePadding: 0,
+
+        },
+        1200:{
+			stagePadding: 380,
+        },
+        1500:{
+			stagePadding: 520,
+        }
+    }
+})
+
+/* project2-active */
+$('.project2-active').owlCarousel({
+    loop:true,
+	margin:0,
+    nav:false,
+	dots:false,
+	autoplay:false,
+	navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+})
+
+/* testimonial-active */
+$('.testimonial-active').owlCarousel({
+    loop:true,
+    nav:true,
+	dots:false,
+	autoplay:true,
+	navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+})
+
+
+/* testimonial2-active */
+$('.testimonial2-active').owlCarousel({
+    loop:true,
+    nav:true,
+	dots:false,
+	autoplay:true,
+	navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+})
+
+
+/* testimonial3-active */
+$('.testimonial3-active').owlCarousel({
+    loop:true,
+    nav:true,
+	dots:false,
+	autoplay:true,
+	navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        992:{
+            items:1
+        },
+        1200:{
+            items:1
+        }
+    }
+})
+
+
+/* counter */
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		// use outer width of grid-sizer for columnWidth
+		columnWidth: 1,
+	  }
+	});
+	// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+});
+
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+
+
+// scrollToTop
+$.scrollUp({
+	scrollName: 'scrollUp', // Element ID
+	topDistance: '300', // Distance from top before showing element (px)
+	topSpeed: 300, // Speed back to top (ms)
+	animation: 'fade', // Fade, slide, none
+	animationInSpeed: 200, // Animation in speed (ms)
+	animationOutSpeed: 200, // Animation out speed (ms)
+	scrollText: '<i class="fas fa-angle-up"></i>', // Text for element
+	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
+
+// WOW active
+new WOW().init();
+
+
+// map
+function basicmap() {
+	// Basic options for a simple Google Map
+	// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+	var mapOptions = {
+		// How zoomed in you want the map to start at (always required)
+		zoom: 11,
+		scrollwheel: false,
+		// The latitude and longitude to center the map (always required)
+		center: new google.maps.LatLng(40.6700, -73.9400), // New York
+		// This is where you would paste any style found on Snazzy Maps.
+		styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+	};
+	// Get the HTML DOM element that will contain your map
+	// We are using a div with id="map" seen below in the <body>
+	var mapElement = document.getElementById('contact-map');
+
+	// Create the Google Map using our element and options defined above
+	var map = new google.maps.Map(mapElement, mapOptions);
+
+	// Let's also add a marker while we're at it
+	var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(40.6700, -73.9400),
+		map: map,
+		title: 'Cryptox'
+	});
+}
+if ($('#contact-map').length != 0) {
+	google.maps.event.addDomListener(window, 'load', basicmap);
+}
+
+})(jQuery);
